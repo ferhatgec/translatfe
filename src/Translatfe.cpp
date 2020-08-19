15858201a617dc5cc6d3c2cef0b0abb7a154cf76
ferhatgec <ferhatgectao@gmail.com>
2020-08-19 08:08:50 +0300

    Added: Translate() function.

--- src/Translatfe.cpp
+++ src/Translatfe.cpp
@@ -12,25 +12,44 @@
 #include <english/german/basics.hpp>
 #include <Translatfe.hpp>
 
-int main(int argc, char **argv) {
-    // ("Welcome back.")
-    std::string source;
-    std::cout << "Fegeya Translatfe - " << TRANSLATFE_VERSION << "\nWrite [English to German]: ";
-    
-    std::getline(std::cin, source);
-
-    std::istringstream stream(source);
+void Translate(std::string source) {
     std::string word;
-    
+    std::istringstream stream(source);
     std::cout /*<< "English : " << source*/ << "German : ";
     while (stream >> word) {
         const auto &t = translations.find(word);
 
-        if (t != translations.end()) { // Found
+        if (t != translations.end()) // Found
             std::cout << t->second << " ";
-        } else { 
+        else
             std::cout << word << " ";
-    	}
+    }
+}
+
+int main(int argc, char **argv) {
+    std::string copy_arg, reg;
+   
+    if(argc > 1) {
+        for(int i = 2; i < argc; i++) {
+	    std::string arg(argv[i]);
+	    reg.append(argv[i]);
+	    reg.append(" ");
+	    copy_arg = argv[1];
+	}
+	
+    } else {
+        // HelpFunction()	
+    }
+
+    if(copy_arg.substr(0, 2) == "--") {
+	   if(copy_arg == "--etog") // English to German
+		Translate(reg);
+	   /*else if(reg == "--h" || reg == "--help")
+		// HelpFunction()
+	   else if(reg == "--v" || reg == "--version")
+		// VersionFunction()
+	   else
+	        // HelpFunction()*/
     }
 
     std::cout << "\n";
